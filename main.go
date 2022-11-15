5f20338efcba9f146adab7fd625615d24925dd35
iiForget <101644667+iiForget@users.noreply.github.com>
2022-11-15 18:19:50 +0800

    raname

--- main.go
+++ main.go
@@ -257,7 +257,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "花酱",),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
